ba77a93bbb42f033c1991e62774ac23318fd43dc
man-disco <109635328+man-disco@users.noreply.github.com>
2022-08-06 19:14:51 +0000

    Add files via upload

---
+++ imc.py
@@ -0,0 +1,32 @@
+# Calculadora de índice de massa corpórea python.
+# Feito por João Lucas, 04/08/2022.
+# imc menor que 18,5 = magro
+# imc entre 18,5 e 24,9 = normal
+# imc entre 25,0 e 29,9 = sobrepeso
+# imc entre 30,0 e 39,9 = obesidade
+# imc maior que 40 = obesidade grave
+
+peso = int(input('Digite seu peso: '))
+altura = float(input('Digite sua altura: '))
+
+imc = peso // (altura ** 2)
+
+print(imc)
+
+if imc <= 18.5:
+    print('Você é magro, ' + 'seu imc mede ' + str(imc))
+
+elif imc <= 18.5 or imc <= 24.9:
+    print('Você está com o peso normal, ' + 'seu imc mede ' + str(imc))
+
+elif imc <= 25.0 or imc <= 29.9:
+    print('Você está acima do peso normal, ' + 'seu imc mede ' + str(imc))
+
+elif imc <= 24.9 or imc <= 30.0:
+    print('Você está com pré-obesidade, ' + 'seu imc mede ' + str(imc))
+
+elif imc > 30.0:
+    print('Você está com obesidade mórbida, ' + 'seu imc mede ' + str(imc))
+
+else:
+    print('Você não digitou o valor corretamente')
